1b78a49e0926c647c66a69f32fdb6e05c259819f
TaSigmund <64551452+TaSigmund@users.noreply.github.com>
2021-05-20 17:04:00 -0400

    can type in signup form

--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from './Header';
 import {useHistory} from 'react-router-dom'
 
@@ -8,6 +8,12 @@ import {useHistory} from 'react-router-dom'
 function UserSignUp(){
 
     let history = useHistory();
+    
+    const [firstNameField, setFirstNameField] = useState("");
+    const [lastNameField, setLastNameField] = useState("");
+    const [emailField, setEmailField] = useState("");
+    const [passwordField, setPasswordField] = useState("");
+    const [confirmPasswordField, setConfirmPasswordField] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -22,15 +28,45 @@ function UserSignUp(){
                     <h2>Sign Up</h2>
                     <form onSubmit={handleSubmit}>
                         <label for="firstName">First Name</label>
-                        <input id="firstName" name="firstName" type="text" value=""/>
+                        <input 
+                            id="firstName" 
+                            name="firstName" 
+                            type="text" 
+                            value={firstNameField}
+                            onChange={e => setFirstNameField(e.target.value)}
+                        />
                         <label for="lastName">Last Name</label>
-                        <input id="lastName" name="lastName" type="text" value=""/>
+                        <input 
+                            id="lastName" 
+                            name="lastName" 
+                            type="text" 
+                            value={lastNameField}
+                            onChange={e => setLastNameField(e.target.value)}
+                        />
                         <label for="emailAddress">Email Address</label>
-                        <input id="emailAddress" name="emailAddress" type="email" value=""/>
+                        <input 
+                            id="emailAddress" 
+                            name="emailAddress" 
+                            type="email" 
+                            value={emailField}
+                            onChange={e => setEmailField(e.target.value)}
+                        />
                         <label for="password">Password</label>
-                        <input id="password" name="password" type="password" value=""/>
+                        <input 
+                            id="password" 
+                            name="password" 
+                            type="password" 
+                            value={passwordField}
+                            onChange={e => setPasswordField(e.target.value)}
+                        />
                         <label for="confirmPassword">Confirm Password</label>
-                        <input id="confirmPassword" name="confirmPassword" type="password" value=""/>
+                        <input 
+                            id="confirmPassword" 
+                            name="confirmPassword" 
+                            type="password" 
+                            value={confirmPasswordField}
+                            onChange={e => setConfirmPasswordField(e.target.value)}
+                            />
                         <button className="button" type="submit">Sign Up</button><button className="button button-secondary">Cancel</button>
                     </form>
                     <p>Already have a user account? Click here to <a href="sign-in.html">sign in</a>!</p>
