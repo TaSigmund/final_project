281e170647e87e24f73f4a7c3441dbee0f20dfee
TaSigmund <64551452+TaSigmund@users.noreply.github.com>
2021-05-24 10:34:52 -0400

    bugfix signin

--- api/auth-user.js
+++ api/auth-user.js
@@ -6,8 +6,9 @@ const bcrypt = require('bcryptjs');
 // Middleware to authenticate the request using Basic Auth.
 exports.authenticateUser = 
 async (req, res, next) => {
-  const credentials = auth(req); //parse user authorization header
-  atob(credentials); //decode base-64 encoded string
+
+const credentials = auth(req); //parse user authorization header
+  
   let message;
   if(credentials){
       const user = await User.findOne({where: {emailAddress: credentials.name}})
